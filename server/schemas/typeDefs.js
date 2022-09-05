b1645688e312f3f24385e687fbbda44b2fdce9ea
sivanagar <sivanagar@gmail.com>
2022-09-05 11:19:43 -0700

    basic typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,23 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    _id:ID
+    username: String
+    email: String
+    events: [Event]
+  }
+
+  type Event {
+    _id: ID
+    title: String
+    address: String
+    description: String
+    owner: String
+    capacity: Int
+    
+  }
+
   type Query {
     hello: String
   }
