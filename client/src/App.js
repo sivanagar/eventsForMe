3e24507e7ce43a4f19c7fd4d6e5d176934c972a7
Lady Gaga <jessiezheqi.li@gmail.com>
2022-09-12 20:59:28 -0700

    Add Dashboard.js skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,16 @@ import {
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 import Home from "./pages/Home/Home";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
+
 import NoMatch from "./pages/NoMatch";
 
-import { BrowserRouter } from 'react-router-dom'
-import {
-  Routes,
-  Route,
-} from "react-router";
-
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 //establish a new link to the GraphQL server
 const httpLink = createHttpLink({
@@ -35,21 +33,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
           {/* <Home /> */}
-
           <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="sign-up" element={<Signup />}></Route>
-        <Route path="*" element={<NoMatch />}></Route>
-      </Routes>
-      </BrowserRouter>
-          
+            <Routes>
+              <Route index element={<Home />}></Route>
+              <Route path="login" element={<Login />}></Route>
+              <Route path="sign-up" element={<Signup />}></Route>
+              <Route path="dashboard" element={<Dashboard />}></Route>
+              <Route path="*" element={<NoMatch />}></Route>
+            </Routes>
+          </BrowserRouter>
         </div>
         <Footer />
       </div>
