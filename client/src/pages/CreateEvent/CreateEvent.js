703519812b2e26cba83647bad2616e56416f3d32
Lady Gaga <jessiezheqi.li@gmail.com>
2022-09-19 23:32:11 -0700

    Update CreateEvent function name

--- client/src/pages/CreateEvent/CreateEvent.js
+++ client/src/pages/CreateEvent/CreateEvent.js
@@ -4,7 +4,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 
-const Signup = () => {
+const CreateEvent = () => {
   const [formState, setFormState] = useState({
     username: "",
     email: "",
@@ -77,4 +77,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default CreateEvent;
