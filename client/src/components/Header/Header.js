762ea34b5c7d6d01f6646980cc5a87d8251c35bf
robyng <robyngraham01@gmail.com>
2022-10-31 09:03:45 -0700

    add login, sign  up, Logout Link to in header. update page links to Link to.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        
         <a href="/">
-        <h1>Events For Me</h1>
+          <h1>Events For Me</h1>
         </a>
-        
-
-      
-        
+        <nav>
+          <Link to="sign-up">Sign Up</Link>
+          <Link to="login">Login</Link>
+          <Link to="/">Logout</Link>
+        </nav>
       </div>
     </header>
   );
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import {ADD_USER} from '../../utils/mutations'
-import Auth from '../../utils/auth';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 const Signup = () => {
-  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   const [addUser, { error }] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-    
+
     setFormState({
       ...formState,
       [name]: value,
@@ -23,20 +28,21 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState, username: formState["email"] },
       });
-      Auth.saveInfo(data.addUser.user)
+      Auth.saveInfo(data.addUser.user);
       Auth.login(data.addUser.token);
-
     } catch (e) {
       console.error(e);
     }
   };
 
   return (
-   <div> 
-    <h3>Start selling tickets for your event. Sign up below.</h3>
-    <p>Already have an account? <a href="/login">Login</a>.</p>
-            <form onSubmit={handleFormSubmit}>
-              {/* <input
+    <div>
+      <h3>Start selling tickets for your event. Sign up below.</h3>
+      <p>
+        Already have an account? <Link to="/login">Login</Link>.
+      </p>
+      <form onSubmit={handleFormSubmit}>
+        {/* <input
                 className='form-input'
                 placeholder='Your username'
                 name='username'
@@ -45,42 +51,42 @@ const Signup = () => {
                 value={formState.username}
                 onChange={handleChange}
               /> */}
-              <label>Your Email</label>
-              <input
-                className='form-input'
-                // placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
+        <label>Your Email</label>
+        <input
+          className="form-input"
+          // placeholder='Your email'
+          name="email"
+          type="email"
+          id="email"
+          value={formState.email}
+          onChange={handleChange}
+        />
 
-              <label>Create Password</label>
-              <input
-                className='form-input'
-                // placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <label>Confirm Password</label>
-                            <input
-                className='form-input'
-                // placeholder='******'
-                name='confirmPassword'
-                type='confirmPassword'
-                id='confirmPassword'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-            </form>
-            </div>
+        <label>Create Password</label>
+        <input
+          className="form-input"
+          // placeholder='******'
+          name="password"
+          type="password"
+          id="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+        <label>Confirm Password</label>
+        <input
+          className="form-input"
+          // placeholder='******'
+          name="confirmPassword"
+          type="confirmPassword"
+          id="confirmPassword"
+          value={formState.password}
+          onChange={handleChange}
+        />
+        <button className="btn d-block w-100" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
