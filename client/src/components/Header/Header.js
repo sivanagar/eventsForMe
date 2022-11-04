9fb62e5bc0878916330eb921467285be8eb228aa
robyng <robyngraham01@gmail.com>
2022-11-04 11:39:08 -0700

    logout button onclick remove auth token.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Header = () => {
+  /*for log out kill session*/
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -16,7 +22,9 @@ const Header = () => {
           <Link to="create-event">Create Event</Link>
           <Link to="my-tickets">My Tickets</Link>
           <Link to="account">Account Details</Link>
-          <Link to="/">Log Out</Link>
+          <a href="/" onClick={logout}>
+            Log Out
+          </a>
         </nav>
       </div>
     </header>
