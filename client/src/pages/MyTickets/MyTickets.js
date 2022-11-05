9a666b389c2fb9ecbd305343e00f11eca2c26d52
Lady Gaga <jessiezheqi.li@gmail.com>
2022-11-04 17:36:41 -0700

    Add basic style to MyTickets map

--- client/src/pages/MyTickets/MyTickets.js
+++ client/src/pages/MyTickets/MyTickets.js
@@ -7,25 +7,31 @@ const MyTickets = () => {
   return (
     <div>
       <div className="flex-row">
-        <h3>My Tickets</h3>
-        <table>
-          <tr>
-            <th>Event</th>
-            <th>Time</th>
-            <th>Quantity</th>
-          </tr>
-        </table>
-        {myTicketsSeed.map(({ eventName, time, location, quantity }) => (
-          <table key={eventName}>
+        <div className="col-12">
+          <h3>My Tickets</h3>
+        </div>
+        <div className="col-12 flex-row">
+          <table className="col-12 justify-space-between">
             <tr>
-              <td>{eventName}</td>
-              <td>
-                {time}, {location}
-              </td>
-              <td>{quantity}</td>
+              <th>Event</th>
+              <th>Time and Location</th>
+              <th>Quantity</th>
             </tr>
           </table>
-        ))}
+        </div>
+        <div className="col-12 flex-row">
+          {myTicketsSeed.map(({ eventName, time, location, quantity }) => (
+            <table key={eventName} className="col-12 justify-flex-start">
+              <tr>
+                <td>{eventName}</td>
+                <td>
+                  {time}, {location}
+                </td>
+                <td>{quantity}</td>
+              </tr>
+            </table>
+          ))}
+        </div>
       </div>
     </div>
   );
