6677172027834ca70f84160c2b56aaa57dcb09f6
mogannam <joemogannam@gmail.com>
2022-11-27 18:38:39 -0800

    Merge Develop & file-upload-unique

--- client/src/components/FileUpload/index.js
+++ client/src/components/FileUpload/index.js
@@ -28,18 +28,30 @@ function FileUpload(props) {
   
   const [progress , setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [eventResponse, setEventResponse] = useState(null);
 
   const handleFileInput = (e) => {
       setSelectedFile(e.target.files[0]);
   }
 
-  const uploadFile = (file) => {
+  const uploadFile = (_fileObject, _DirName) => {
+
+    const formatFileName = _fileObjectB => {
+      let tempName = ""
+      tempName = `${_fileObjectB.name}_${(new Date().toJSON())}`
+      return tempName
+    }
+
+    const uniqueFileName = formatFileName(_fileObject)
 
       const params = {
           ACL: 'public-read',
-          Body: file,
+          Body: _fileObject,
           Bucket: config.bucketName,
-          Key: `${config.dirName}\\${file.name}`,
+          // if for debugging purposes we want to override the dirName, we can pass the directory name 
+          // as a config variable
+          //Key: `${config.dirName  }\\${uniqueFileName}`,
+          Key: `${_DirName  }\\${uniqueFileName}`,
           
       };
 
@@ -52,14 +64,45 @@ function FileUpload(props) {
           })
   }
 
+  const submitEventHelper = async (event) => {
+    // submit event
+    event.preventDefault();
+    
+    try {
+      const tempData = await props.ParrentHandleFormSubmit()
+      
+      if(tempData){ // if the event submitted sucesfully
+        setEventResponse(tempData)
+        const  dirName2 = tempData.addEvent._id   // recieve a nested object like {addEvent : {_id:123, name: "some string"}}
+        // destructure only the data we care about the id
+        
+      
+        if (selectedFile) // if we suscesfully selected a file, submit the file
+          uploadFile(selectedFile, dirName2) // upload the file to aws using the event id as a directory name
+
+      }
+      // ToDo Determine else clause when the event fails to submit
+      
+    }
+    catch (e){
+      console.log(`Custom error: ${e}`)
+      console.error(e)
+
+    }
+
+    // Upload img
+  }
+
 
   return <div>
       <div>Native SDK File Upload Progress is {progress}%</div>
       <input type="file" onChange={handleFileInput}/>
-      <a href="#" onClick={() => { uploadFile(selectedFile) ; props.ParrentHandleFormSubmit() }}> Upload to S3</a>
+      <button  onClick={ submitEventHelper }> Submit Event &#38; Photo</button>
+
   </div>
 }
 
 export default FileUpload;
 
+// uploadFile(selectedFile) ;  props.ParrentHandleFormSubmit().then(eventData => {console.log(JSON.stringify(eventData))}).catch(error=> {console.log(`custom error ${error}`)}) ; console.log("Event Submitted ")
 //uploadFile(selectedFile) ; props.ParrentHandleFormSubmit(props.ParrentformState)
