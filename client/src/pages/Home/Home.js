e3c7e310e1f3a532329a9faaadec7956376d2084
lijessie <li.jessie@gmail.com>
2022-12-08 19:29:30 -0800

    Add logic to hide sign-up button when logged in

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,16 +2,16 @@ import React from "react";
 import Button from "@mui/material/Button";
 import FileUpload from "../../components/FileUpload";
 import CreateEvent from "../CreateEvent/CreateEvent";
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const Home = () => {
+  const loggedIn = Auth.loggedIn();
   return (
     <div>
-        <a href="/sign-up" >
-        <Button variant="outlined">SIGN UP</Button>
-        </a> 
-    {Auth.loggedIn() ? <CreateEvent/> : "" }
-
+      <a href="/sign-up">
+        {loggedIn ? null : <Button variant="outlined">SIGN UP</Button>}
+      </a>
+      {loggedIn ? <CreateEvent /> : ""}
     </div>
   );
 };
