60e4444d160fac56cad621fbbf0cee31dd970493
mogannam <joemogannam@gmail.com>
2022-12-31 21:57:32 -0800

    Refactor Dashboard/EventList; add code to pull all events into dashboard ; ToDo: load the images

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,12 @@ import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard/Dashboard";
 import FileUpload from "./components/FileUpload";
 import FileDownload from "./components/FileDownload";
-
+//import { EventList } from "./components/EventList/EventList";
 import Home from "./pages/Home/Home";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import CreateEvent from "./pages/CreateEvent/CreateEvent";
-import { EventList } from "./pages/EventList/EventList";
+
 import MyTickets from "./pages/MyTickets/MyTickets";
 import Account from "./pages/AccountDetails/AccountDetails";
 
@@ -51,7 +51,7 @@ function App() {
               <Route path="sign-up" element={<Signup />}></Route>
               <Route path="dashboard" element={<Dashboard />}></Route>
               <Route path="create-event" element={<CreateEvent />}></Route>
-              <Route path="events" element={<EventList />}></Route>
+              
               <Route path="file-upload" element={<FileUpload />}></Route>
               <Route path="file-download" element={<FileDownload />}></Route>
               <Route path="my-tickets" element={<MyTickets />}></Route>
